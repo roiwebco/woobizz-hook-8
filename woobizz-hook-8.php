0b25bb42bfafc7fe1e7042341e2553bcda38ff05
roiwebco <30891172+roiwebco@users.noreply.github.com>
2018-06-09 16:31:18 +0200

    Update translation

--- woobizz-hook-8.php
+++ woobizz-hook-8.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Woobizz Hook 8
 Plugin URI: http://woobizz.com
-Description: Add empty cart button on cart page
-Author: Woobizz
+Description: Add a button to empty the entire cart on the cart page
+Author: WOOBIZZ.COM
 Author URI: http://woobizz.com
-Version: 1.0.0
+Version: 1.0.1
 Text Domain: woobizzhook8
 Domain Path: /lang/
 */
@@ -16,16 +16,6 @@ add_action( 'plugins_loaded', 'woobizzhook8_load_textdomain' );
 function woobizzhook8_load_textdomain() {
   load_plugin_textdomain( 'woobizzhook8', false, basename( dirname( __FILE__ ) ) . '/lang' ); 
 }
-//Check if WooCommerce is active
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	//echo "woocommerce is active";
-	 add_action ('woocommerce_cart_actions','woobizzhook8_add_empty_cart_button');
-	 add_action( 'init', 'woobizzhook8_empty_cart_url' );
-}else{
-	//Show message on admin
-	//echo "woocommerce is not active";
-	add_action( 'admin_notices', 'woobizzhook8_admin_notice' );
-}
 //Add Hook 8
 //Empty cart button
 function woobizzhook8_add_empty_cart_button() { 
@@ -34,6 +24,7 @@ function woobizzhook8_add_empty_cart_button() {
 	echo '<a class="button" href="'.$woocommerce->cart->get_cart_url().'?empty-cart">'.$woobizzhook8_empty_cart_txt.'</a>';
   
 }
+add_action ('woocommerce_cart_actions','woobizzhook8_add_empty_cart_button');
 //Empty cart URL
 function woobizzhook8_empty_cart_url() {
 	if ( isset( $_GET['empty-cart'] ) ) {
@@ -41,11 +32,4 @@ function woobizzhook8_empty_cart_url() {
 		$woocommerce->cart->empty_cart();
 	}
 }
-//Hook8 Notice
-function woobizzhook8_admin_notice() {
-    ?>
-    <div class="notice notice-error is-dismissible">
-        <p><?php _e( 'Woobizz Hook 8 needs WooCommerce to work properly, If you do not use this plugin you can disable it!', 'woobizzhook8' ); ?></p>
-    </div>
-    <?php
-}
+add_action( 'init', 'woobizzhook8_empty_cart_url' );
